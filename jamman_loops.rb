67c517c7b56e6b5deddf9980c634bc6ae46d6323
dean <dean@splash.local>
2009-08-17 23:26:42 -0500

    more explicit loop_num -> bpm

--- jamman_loops.rb
+++ jamman_loops.rb
@@ -10,7 +10,7 @@
 #       LOOP.XML   # the loop definition file with bpm, stop mode, loop or one-shot options
 #    LOOP02/...
 # A loop definition file looks like this, with %d replaced with the num of ticks per beat 44100 / bpm * 60
-loop_def = 
+loop_template = 
   %q{<?xml version="1.0" encoding="utf-8" ?>
 <JamManLoopSetup>
 	<LoopMode>Loop</LoopMode>
@@ -20,18 +20,18 @@ loop_def =
 	<StopMode>Stop</StopMode>
 </JamManLoopSetup>
 }
-# For loops in this range
+
+tempo_from_loop_num = lambda{ |loop_num| loop_num * 2 }
 puts "Writing loops into ./LOOP*/*.xml"
 
-(30..90).each do |bpm|
-  Dir.mkdir(dir_name ="LOOP#{bpm}") rescue nil
+# For loops in this range
+(30..90).each do |loop_num|
+  Dir.mkdir(dir_name ="LOOP#{loop_num}") rescue nil
   File.open("#{dir_name}/LOOP.XML", "w") do |f|
     # we divide the ticks in half to double the tempo of each loop relative to its loop number
-    ticks = (44100.0 / bpm.to_f)*60 / 2
-    f.puts(loop_def % ticks)
+    bpm = tempo_from_loop_num.call(loop_num)
+    ticks = (44100.0 / bpm)*60
+    f.puts(loop_template % ticks)
   end  
-  puts "Wrote LOOP#{bpm}/LOOP.XML"
+  puts "Wrote LOOP#{loop_num}/LOOP.XML"
 end
-
-#puts loop_def.split("\n").join("\r\n")
-# Create the folder
